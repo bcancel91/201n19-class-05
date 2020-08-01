81c4ad76d2f71c91f94ef68134a1a6cddef6d96d
Brian Cancel <brian.e.cancel@gmail.com>
2020-08-01 19:11:56 -0400

    problem 2 complete

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,16 +8,16 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) {
+function sum(a, b, c) {
   //eslint-disable-line
-  var theSum = a + b;
+  var theSum = a + b + c;
   var theString = `The sum of ${a} and ${b} is ${theSum}.`;
   // console.log(theSum, theString);
   return [theSum, theString];
 }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+// testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -30,16 +30,16 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) {
+function multiply(a, b, c) {
   //eslint-disable-line
-  var ans = a * b;
+  var ans = a * b * c;
   var str = `The product of ${a} and ${b} is ${ans}.`;
   // console.log(ans, str);
   return [ans, str];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5, 9);
+// testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -57,10 +57,21 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) {
   //eslint-disable-line
+  var sumNums = sum(a, b, c)[0];
+  // console.log(sumNums);
+
+  var productNums = multiply(a, b, c)[0];
+  // console.log(productNums);
+
+  var sumStr = `${a} and ${b} and ${c} sum to ${sumNums}.`;
+  // console.log(sumStr);
+  var productStr = `The product of ${a} and ${b} and ${c} is ${productNums}.`;
+  // console.log(productStr);
+  return [sumNums, productNums, sumStr, productStr];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
